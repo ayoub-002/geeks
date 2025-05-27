34b71e9774d4be52154fbf5d84d1d379f3f799d2
Ayoub Zaddi <ayoubzaddi177@gmail.com>
2025-05-27 11:55:17 +0100

    exercise day02 week04

---
+++ week04/day02/exercise/exercise.js
@@ -0,0 +1,68 @@
+// Exercise 1
+const colors1 = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+colors1.forEach((color, index) => console.log(`${index + 1}# choice is ${color}`));
+console.log(colors1.includes("Violet") ? "Yeah" : "No...");
+
+// Exercise 2
+const colors2 = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th", "st", "nd", "rd"];
+colors2.forEach((color, index) => {
+  let suffix = ordinal[(index + 1)] || ordinal[0];
+  console.log(`${index + 1}${suffix} choice is ${color}`);
+});
+
+// Exercise 3
+const fruits = ["apple", "orange"];
+const vegetables = ["carrot", "potato"];
+const result = ['bread', ...vegetables, 'chicken', ...fruits];
+console.log(result);
+
+const country = "USA";
+console.log([...country]);
+
+let newArray = [...[,,]];
+console.log(newArray);
+
+// Exercise 4
+const users = [
+  { firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+  { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+  { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+  { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+  { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+  { firstName: 'Wes', lastName: 'Reid', role: 'Instructor' },
+  { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}
+];
+
+const welcomeStudents = users.map(user => `Hello ${user.firstName}`);
+console.log(welcomeStudents);
+
+const fullStackResidents = users.filter(user => user.role === 'Full Stack Resident');
+console.log(fullStackResidents);
+
+const lastNames = users
+  .filter(user => user.role === 'Full Stack Resident')
+  .map(user => user.lastName);
+console.log(lastNames);
+
+// Exercise 5
+const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+const fullSentence = epic.reduce((acc, word) => acc + " " + word);
+console.log(fullSentence);
+
+// Exercise 6
+const students = [
+  {name: "Ray", course: "Computer Science", isPassed: true}, 
+  {name: "Liam", course: "Computer Science", isPassed: false}, 
+  {name: "Jenner", course: "Information Technology", isPassed: true}, 
+  {name: "Marco", course: "Robotics", isPassed: true}, 
+  {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+  {name: "Jamie", course: "Big Data", isPassed: false}
+];
+
+const passedStudents = students.filter(s => s.isPassed);
+console.log(passedStudents);
+
+students
+  .filter(s => s.isPassed)
+  .forEach(s => console.log(`Good job ${s.name}, you passed the course in ${s.course}`));
