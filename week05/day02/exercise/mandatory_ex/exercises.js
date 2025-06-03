01e79a4a28b23005f8341a4aa9b8b77490aefe35
Ayoub Zaddi <ayoubzaddi177@gmail.com>
2025-06-03 15:10:12 +0100

    day02 week05 mandatory exercises

--- week05/day02/exercise/mandatory_ex/exercises.js
+++ week05/day02/exercise/mandatory_ex/exercises.js
@@ -1,9 +1,11 @@
 //Exercise 1
 function fetchHilariousGifs() {
-  const url = "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+  const apiKey = "hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+  const url = `https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=${apiKey}`;
+
   fetch(url)
     .then(response => {
-      if (!response.ok) throw new Error("Network response was not ok");
+      if (!response.ok) throw new Error("Failed to fetch hilarious gifs");
       return response.json();
     })
     .then(data => console.log("Exercise 1 - Hilarious Gifs:", data))
@@ -12,10 +14,12 @@ function fetchHilariousGifs() {
 
 //Exercise 2
 function fetchSunGifs() {
-  const url = "https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+  const apiKey = "hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+  const url = `https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=${apiKey}`;
+
   fetch(url)
     .then(response => {
-      if (!response.ok) throw new Error("Network response was not ok");
+      if (!response.ok) throw new Error("Failed to fetch sun gifs");
       return response.json();
     })
     .then(data => console.log("Exercise 2 - Sun Gifs:", data))
@@ -26,7 +30,7 @@ function fetchSunGifs() {
 async function fetchStarship() {
   try {
     const response = await fetch("https://www.swapi.tech/api/starships/9/");
-    if (!response.ok) throw new Error("Network response was not ok");
+    if (!response.ok) throw new Error("Failed to fetch starship data");
     const data = await response.json();
     console.log("Exercise 3 - Starship:", data.result);
   } catch (error) {
@@ -53,7 +57,7 @@ function analyzeAsyncFunction() {
   asyncCall();
 }
 
-//Call all functions to run them
+// Run all exercises
 fetchHilariousGifs();
 fetchSunGifs();
 fetchStarship();
