b2eed8fac26ccf056f9aff171c36f32c78aa2202
Ayoub Zaddi <ayoubzaddi177@gmail.com>
2025-06-09 11:17:44 +0100

    week05

--- week05/day01/exercise/exercise.js
+++ week05/day01/exercise/exercise.js
@@ -2,21 +2,22 @@
 function compareToTen(num) {
   return new Promise((resolve, reject) => {
     if (num <= 10) {
-      resolve(`${num} is less than or equal to 10`);
+      resolve("The number is less than or equal to 10");
     } else {
-      reject(`${num} is greater than 10`);
+      reject("The number is greater than 10");
     }
   });
 }
 
-//Test cases for compareToTen
+// Test
 compareToTen(15)
-  .then(result => console.log("Exercise 1:", result))
-  .catch(error => console.log("Exercise 1:", error));
+  .then(result => console.log("// Exercise 1:", result))
+  .catch(error => console.log("// Exercise 1:", error));
 
 compareToTen(8)
-  .then(result => console.log("Exercise 1:", result))
-  .catch(error => console.log("Exercise 1:", error));
+  .then(result => console.log("// Exercise 1:", result))
+  .catch(error => console.log("// Exercise 1:", error));
+
 
 
 //Exercise 2: Promises
@@ -26,16 +27,16 @@ const successPromise = new Promise((resolve) => {
   }, 4000);
 });
 
-//Test for successPromise
-successPromise.then(result => console.log("Exercise 2:", result));
+successPromise.then(result => console.log("// Exercise 2:", result));
+
 
 
 //Exercise 3: Resolve & Reject
 const resolvedPromise = Promise.resolve(3);
 const rejectedPromise = Promise.reject("Boo!");
 
-//Test for resolvedPromise
-resolvedPromise.then(value => console.log("Exercise 3 (resolve):", value));
+// Handle the resolved promise
+resolvedPromise.then(value => console.log("// Exercise 3 (resolve):", value));
 
-//Test for rejectedPromise
-rejectedPromise.catch(error => console.log("Exercise 3 (reject):", error));
+// Handle the rejected promise
+rejectedPromise.catch(error => console.log("// Exercise 3 (reject):", error));
